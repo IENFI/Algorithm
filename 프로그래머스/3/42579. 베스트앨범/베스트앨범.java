d02968a088c5d3c9c74b22e87926c1bfdaca85b6
IENFI <138753711+IENFI@users.noreply.github.com>
2026-08-05 17:43:24 +0900

    [level 3] Title: 베스트앨범, Time: 15.48 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+class Solution {
+	static HashMap<String, ArrayList<Song>> map;
+	static HashMap<String, Integer> playsOfGenres;
+	static ArrayList<Integer> result;
+
+	public static int[] solution(String[] genres, int[] plays) {
+		map = new HashMap<>();
+		playsOfGenres = new HashMap<>();
+		result = new ArrayList<>();
+		
+		for (int i = 0; i < genres.length; i++) {
+//			map.computeIfAbsent(genres[i], k -> new ArrayList<>())
+//				.add(new Song(i, genres[i], plays[i]));
+			map.putIfAbsent(genres[i], new ArrayList<>());
+			map.get(genres[i]).add(new Song(i, genres[i], plays[i]));
+
+			playsOfGenres.put(genres[i], playsOfGenres.getOrDefault(genres[i], 0) + plays[i]);
+		}
+
+		for (ArrayList<Song> songs : map.values()) {
+			songs.sort(Comparator.comparingInt((Song song) -> song.play).reversed().thenComparingInt(song -> song.num));
+		}
+		
+		ArrayList<String> orderedGenre =
+			    new ArrayList<>(playsOfGenres.keySet());
+
+		
+		orderedGenre.sort((a,b) -> playsOfGenres.get(b) - playsOfGenres.get(a));
+		
+		for(String genre: orderedGenre) {
+			ArrayList<Song> songs = map.get(genre);
+			songs.stream().limit(2).forEach(song -> result.add(song.num));
+		}
+
+		return  result.stream()
+		        .mapToInt(Integer::intValue)
+		        .toArray();
+	}
+
+	public static class Song {
+		int num;
+		String genre;
+		int play;
+
+		Song(int n, String g, int p) {
+			num = n;
+			genre = g;
+			play = p;
+		}
+	}
+	
+}
