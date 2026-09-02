11c48293255a4992ab04bfd5482a82003c149c71
IENFI <138753711+IENFI@users.noreply.github.com>
2026-09-02 09:30:32 +0900

    [D3] Title: 퍼펙트 셔플, Time: 106 ms, Memory: 32,104 KB -BaekjoonHub

---
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static BufferedReader br;
+	static StringBuilder sb;
+	static StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		// System.setIn(new FileInputStream("res/S3499/sample_input.txt"));
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			String[] arr = new String[N];
+			st = new StringTokenizer(br.readLine());
+			
+			for (int i = 0; i < N; i++) {
+				arr[i] = st.nextToken();
+			}
+			
+			sb.append('#').append(t).append(' ');
+			
+			int center = (int) Math.ceil((float)N/2);
+			
+			for(int i = 0; i < center; i++) {
+				sb.append(arr[i]).append(' ');
+				if (i + center >= N) break;
+				sb.append(arr[i + center]).append(' ');
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+
+}
