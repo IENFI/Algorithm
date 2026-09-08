157f40e1198cd36732802d851d20cf1215293b7b
Minyoung Ha <138753711+IENFI@users.noreply.github.com>
2026-09-09 07:40:08 +0900

    [level 3] Title: 징검다리 건너기, Time: 24.42 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,35 @@
+class Solution {
+    static int[] stones;
+    static int answer;
+    public int solution(int[] stones, int k) {
+        this.stones = stones;
+        binarySearch(1, 200000000, k);
+        return answer;
+    }
+    
+    public void binarySearch(int left, int right, int num) {
+        // overflow 방지
+        int mid = left + (right - left) / 2;
+        if (left > right) {
+            return;
+        }
+        
+        int jump = 1, maxJump = 1;
+        for (int i = 0; i < stones.length; i++) {
+            if (stones[i] < mid) jump++;
+            else {
+                maxJump = Math.max(maxJump, jump);
+                jump = 1;
+            }
+        }
+        maxJump = Math.max(maxJump, jump);
+        
+        if (maxJump <= num) {
+            answer = mid;
+            binarySearch(mid + 1, right, num);
+        }
+        else {
+            binarySearch(left, mid - 1, num);
+        }
+    }
+}
